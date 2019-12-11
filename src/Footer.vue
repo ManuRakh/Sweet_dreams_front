a6e7ee959be02138a8c3ba8be3d21237f84a7ce1
Manucheckhr Rakhimov <manucher5160@gmail.com>
2019-12-11 08:56:43 +0300

    init commit, added little routes, and footer

---
+++ src/Footer.vue
@@ -0,0 +1,19 @@
+
+<template>
+
+  <div id="app2">
+    <FooterComponent />
+  </div>
+  
+</template>
+<script>
+import FooterComponent from './components/FooterComponent.vue'
+
+
+export default {
+  name: 'app2',
+  components: {
+    FooterComponent
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import FooterComponent from './Footer.vue'
 
 Vue.config.productionTip = false
-
+Vue.use(BootstrapVue)
+const NotFound = { template: '<p>Страница не найдена</p>' }
+const routes = {
+  '/': App,
+  '/about': FooterComponent
+}
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
